8de5b935cacaf5d4be87eefcd0813e97b7883e2b
Jeniffer Martins <martins.jeniffer@usp.br>
2020-03-19 21:14:29 -0300

    exercício 4

---
+++ LISTA01/testExpr.java
@@ -0,0 +1,8 @@
+class testExpr {
+    public static void main(String[] args) {
+        //EX04
+        System.out.println("EX04");
+        System.out.println("6 * (2 + 3) / (4 + 5) = "+(6 * (2 + 3) / (4 + 5)));
+        System.out.println("(42 / 7) * 5 + 6 * 3 + 4 = "+((42 / 7) * 5 + 6 * 3 + 4));
+    }
+}
