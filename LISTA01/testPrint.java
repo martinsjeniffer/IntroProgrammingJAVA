c5f6e293f77c73c797a1559a026616af72eeab53
Jeniffer Martins <martins.jeniffer@usp.br>
2020-03-19 21:13:40 -0300

    exercícios 1, 2 e 3

---
+++ LISTA01/testPrint.java
@@ -0,0 +1,26 @@
+class testPrint {
+    public static void main(String[] args) {
+        //EX 01
+        System.out.println("EX01");
+        System.out.println("Estou testando o print");
+        //EX 02
+        System.out.println("\nEX02");
+        System.out.println("Estou\ntestando\no\nprint");
+        //EX 03
+        System.out.println("\nEX03");
+        System.out.println("10 + 3 * 4 = ");
+        System.out.println(10 + 3 * 4);
+        System.out.println("(10 + 3) * 4 = ");
+        System.out.println((10 + 3) * 4);
+        System.out.println("10 + (3 * 4) = ");
+        System.out.println(10 + (3 * 4));
+        System.out.println("23/4 = ");
+        System.out.println(23 / 4);
+        System.out.println("4/23 = ");
+        System.out.println(4 / 23);
+        System.out.println("23 % 4 = ");
+        System.out.println(23 % 4);
+        System.out.println("4 % 23 = ");
+        System.out.println(4 % 23);
+    }
+}
