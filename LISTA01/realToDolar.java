ab6f5bbfdadfc8587485ed598ecc4154a0855138
Jeniffer Martins <martins.jeniffer@usp.br>
2020-03-19 21:14:51 -0300

    exercicio 6

---
+++ LISTA01/realToDolar.java
@@ -0,0 +1,9 @@
+class realToDolar {
+    public static void main(String[] args){
+        //EX05
+        double real = 3F;
+        double dolar = 5.1F;
+
+        System.out.println("R$"+real+" eh equivalente a "+(real/dolar)+" dolares");
+    }
+}
